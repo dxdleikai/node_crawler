cb13119772d31267aa1181aa8eab83f1e7416ecd
dxdleikai <leikai@bravovcloud.com>
2019-03-15 00:36:50 +0800

    modify log information

--- node_cheerio_hupu/index.js
+++ node_cheerio_hupu/index.js
@@ -2,19 +2,13 @@ const superagent = require('superagent');
 //nodejs里一个非常方便的客服端请求代理模块
 const cheerio = require('cheerio');
 //Node.js版的jQuery
-const async = require('async');
 
 const fs = require('fs');
 //fs操作IO
 const url = require('url');
 //url'
-const https = require('https');
 
-const request = require('request');
-const hupuUrl = 'https://bbs.hupu.com/selfie-1';
-
-
-let hupuUrl2;
+const async = require('async');
 
 fs.exists('data', (exists) => {
     if (!exists) {
@@ -24,8 +18,16 @@ fs.exists('data', (exists) => {
     }
 });
 
-let json = [];
-for (let i = 1; i <= 1; i++) {
+let result_json = [];
+let per_page = 40;
+let all_blogs = 0;
+let pages = 5;
+
+let start = 5;
+let end = start + pages;
+
+//虎扑未登录状态只能查看前10页；
+for (let i = start; i <= end; i++) {
     let hupuUrl2 = 'https://bbs.hupu.com/selfie-' + i;
     superagent.get(hupuUrl2)
         .end((err, res) => {
@@ -33,8 +35,8 @@ for (let i = 1; i <= 1; i++) {
                 return console.error(err);
             } else {
                 let $ = cheerio.load(res.text);
+                let blog_length = $('.titlelink>a:first-child').length;
                 $('.titlelink>a:first-child').each((index, element) => {
-                
                     let $element = $(element);
                     let href = url.resolve(hupuUrl2, $element.attr('href'));
                     superagent.get(href)
@@ -42,6 +44,7 @@ for (let i = 1; i <= 1; i++) {
                             if (err) {
                                 console.error(err);
                             } 
+                            all_blogs++;
                             let $ = cheerio.load(res.text);
                             let add = href;
                             let title = $('.bbs-hd-h1>h1').attr('data-title');
@@ -51,56 +54,58 @@ for (let i = 1; i <= 1; i++) {
 
                             let picArr = [];
                             imgs.each((i, e) => {
-                                console.log(`=======${i}=====`, $(e).attr('src'));
                                 let src = $(e).attr('src');
                                 if (!src.endsWith('placeholder.png')) {
                                     picArr.push($(e).attr('src'));
                                 }
                             });
                             
-                            let stad = {
+                            result_json.push({
                                 address: add,
                                 title: title,
                                 ID: txname,
                                 touxiang: tximg,
                                 pics: picArr,
-                            };
-                            
-                            fs.appendFile('result1.json', JSON.stringify(stad), 'utf-8', (err) => {
-                                if (err) {
-                                    throw new Error('appendFile failed...');
-                                }
                             });
                            
+                            if (all_blogs === pages*per_page) {
+                                fs.writeFile('result.json', JSON.stringify(result_json), err => {
+                                    if (err) {
+                                        throw new Error('write result.json failed');
+                                    }
+                                })
+                            }
+                            
+                            if (!title) {
+                                title = all_blogs;
+                            }
+
                             let lujin = `data/${title}/`;
 
-                            ((picArr, lujin) => {
-                                fs.exists(lujin, (exists) => {
-                                    if (!exists) {
-                                        fs.mkdir(lujin, (err) => {
-                                            if (err) {
-                                                throw err;
-                                            }
-                                            async.mapSeries(picArr, (item, callback) => {
-                                                setTimeout(() => {
-                                                    downloadPic(item, `${lujin}/${Date.now()}.jpg`);
-                                                    callback(null, item);
-                                                }, 400);
-                                            }, (err, results) => {});
-                                        });
-                                        console.log('make new directory success');
-                                    } else {
-                                        console.log('this directory is already eixt');
-                                    }
-                                });
-                            })(picArr, lujin);
+                            fs.exists(lujin, (exists) => {
+                                if (!exists) {
+                                    fs.mkdir(lujin, (err) => {
+                                        if (err) {
+                                            throw err;
+                                        }
+                                        async.mapSeries(picArr, (item, callback) => {
+                                            setTimeout(() => {
+                                                downloadPic(item, `${lujin}/${Date.now()}.jpg`);
+                                                callback(null, item);
+                                            }, 400);
+                                        }, (err, results) => {});
+                                    });
+                                    console.log(`make new directory 【${title}】 success`);
+                                } else {
+                                    console.log(`directory【${title}】 is already eixt`);
+                                }
+                            });
                         });
                 });
             }
         })
 }
 
-// let fs = require('fs');
 let Crawler = require('crawler');
 
 let c = new Crawler({
@@ -137,35 +142,13 @@ function requestXhr(url, fileName) {
 function saveAs(data, filename) {
     fs.writeFile(filename, data, 'binary', err => {
         if (err) {
-            console.log('download fail');
+            console.log(`download【${filename}】 fail`);
         } else {
-            console.log('download success');
+            console.log(`download 【${filename}】 success`);
         }
     });
 }
 
 function downloadPic(url, filename) {
     requestXhr(url, filename);
-}
-
-
-function writeLog(log) {
-    let dir = `log`;
-    fs.exists(dir, (exists) => {
-        if (!exists) {
-            fs.mkdir(dir, (err) => {
-                if (err) {
-                    throw err;
-                }
-                
-            });
-        }
-        fs.writeFile(`${dir}/${Date.now()}.log`, log, (err) => {
-            if (err) {
-                console.log('write log failed');
-            } else {
-                console.log('write log success');
-            }
-        });
-    });
 }
